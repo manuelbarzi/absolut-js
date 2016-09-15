1902bbbd2a0d4802a3bb5aa9e0da676c57635a7d
Manuel Barzi <manuelbarzi@nbtfsbd2syy1.tuitravel-ad.net>
2016-09-15 11:00:17 +0200

    update

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@
         var view = new Absolut.View(document.getElementById('view'));
         view.borderWidth(0);
         view.visible(true);
-        view.size(500, 1000);
+        view.size(600, 1000);
         view.location((Absolut.window.width() - view.width()) / 2, 0);
         view.add(new Absolut.Resize(function(event) {
             view.location((Absolut.window.width() - view.width()) / 2, 0);
